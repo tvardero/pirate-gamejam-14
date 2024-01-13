c187fd93d79d386466d535003a58b0de0740858f
Vardo Ternos <tvardero@gmail.com>
2024-01-13 21:19:00 +0200

    Add inputs

---
+++ code/InputActionNames.cs
@@ -0,0 +1,14 @@
+/// <summary>
+/// Input action names.
+/// </summary>
+public static class InputActionNames
+{
+    public const string MoveUp = "move_up";
+    public const string MoveDown = "move_down";
+    public const string MoveLeft = "move_left";
+    public const string MoveRight = "move_right";
+    public const string JoystickCameraUp = "joystick_camera_up";
+    public const string JoystickCameraDown = "joystick_camera_down";
+    public const string JoystickCameraLeft = "joystick_camera_left";
+    public const string JoystickCameraRight = "joystick_camera_right";
+}
