b24011002e182c5e796106125821efc82d41222f
Vardo Ternos <tvardero@gmail.com>
2024-01-13 21:54:37 +0200

    Add interactivity with props

--- code/InputActionNames.cs
+++ code/InputActionNames.cs
@@ -11,4 +11,8 @@ public static class InputActionNames
     public const string JoystickCameraDown = "joystick_camera_down";
     public const string JoystickCameraLeft = "joystick_camera_left";
     public const string JoystickCameraRight = "joystick_camera_right";
+    public const string Sprint = "sprint";
+    public const string Attack = "attack";
+    public const string PrimaryAction = "primary_action";
+    public const string SecondaryAction = "secondary_action";
 }
